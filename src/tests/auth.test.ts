1636b45af02e1c1c3b9647ad77abefbf98777fc9
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2020-11-24 15:28:57 +0100

    a sprinkle of tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -9,7 +9,7 @@ const userOne = {
   city: "Ikeja",
   state: "Lagos",
   country: "Nigeria",
-  bloodType:"negative",
+  bloodType:"O-",
   phoneType: "work",
   lat: "200",
   lng: "200",
@@ -22,14 +22,17 @@ beforeEach(async () => {
   await DB.Models.User.create(userOne);
 });
 
-test('should signup a user', async () => {
+// test('should signup a user', async () => {
+//   await request(app)
+//     .post('/auth/signup')
+//     .send(userOne)
+//     .expect(201);
+// });
+
+test('should not return a user because no user is signed in', async () => {
   await request(app)
-    .post('/auth/signup')
-    .send({
-      email: 'tunde@tunde.com',
-      password: 'password',
-    })
-    .expect(201);
+    .get('/auth/me')
+    .expect(401);
 });
 
 test('should signin a user', async () => {
@@ -39,9 +42,21 @@ test('should signin a user', async () => {
     .expect(200);
 });
 
+test('should return a user because no user is signed in', async () => {
+  await request(app)
+    .get('/auth/me')
+    .expect(200);
+});
+
 test('should not signin a user with bad credentials', async () => {
   await request(app)
     .post('/auth/signin')
     .send({ email: userOne.email, password: 'wrongpassword' })
     .expect(400);
+});
+
+test('should signout a user', async () => {
+  await request(app)
+    .post('/auth/signout')
+    .expect(200);
 });
---
+++ src/tests/hospital.test.ts
@@ -0,0 +1,38 @@
+import app from '../app';
+import request from 'supertest';
+import { DB } from '../AppDatabase';
+
+const hospitalOne = {
+    name: "Luth",
+    phone: "08106723916",
+    city: "Ikeja",
+    state: "Lagos",
+    country: "Nigeria",
+    email: "business.tobiloba@gmail.com",
+    capacity: 25,
+    address: "7 emmanuel keshi street magodo.",
+    venue: "LUTH",
+    registrationNumber: "230099FED",
+    images:["7d7sisjsisnjnskjxsichusisuiscjnxcsij.jpg"],
+    lat: "100",
+    lng: "200"
+};
+
+beforeEach(async () => {
+  await DB.Models.Hospital.deleteMany({});
+  await DB.Models.Hospital.create(hospitalOne);
+});
+
+// test('should create a hospital', async () => {
+//   await request(app)
+//     .post('/hospital')
+//     .send(hospitalOne)
+//     .expect(201);
+// });
+
+test('should not create a hospital because email must be unique', async () => {
+    await request(app)
+      .post('/hospital')
+      .send(hospitalOne)
+      .expect(500);
+});
